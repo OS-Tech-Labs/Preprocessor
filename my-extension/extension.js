476ddeee420efb00fc96a4c6b8f87212586b5310
OmalyaV <119015789+OmalyaV@users.noreply.github.com>
2024-05-29 21:15:17 +0530

    frontend Test

--- my-extension/extension.js
+++ my-extension/extension.js
@@ -17,19 +17,70 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('My-Extension', function () {
+	let helloworldDisposable = vscode.commands.registerCommand('My-Extension', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from My Extension!');
 	});
 
-	context.subscriptions.push(disposable);
+	let webviewDisposable = vscode.commands.registerCommand('My-Extension.openwebview', function () {
+		// Create and show a new webview
+		const panel = vscode.window.createWebviewPanel(
+			'exampleWebview', // Identifies the type of the webview. Used internally
+			'Example Webview', // Title of the panel displayed to the user
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		);
+
+		panel.webview.html = getWebviewContent();
+
+		// Handle messages from the webview
+		panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert':
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			},
+			undefined,
+			context.subscriptions
+		);
+	});
+
+	context.subscriptions.push(helloworldDisposable);
+	context.subscriptions.push(webviewDisposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Example Webview</title>
+	</head>
+	<body>
+            <h1>Hello from the Webview!</h1>
+            <button id="myButton">Click me</button>
+            <script>
+                const vscode = acquireVsCodeApi();
+                document.getElementById('myButton').addEventListener('click', () => {
+                    vscode.postMessage({
+                        command: 'alert',
+                        text: 'Button clicked!'
+                    });
+                });
+            </script>
+        </body>
+	</html>`;
+}
 module.exports = {
 	activate,
 	deactivate
